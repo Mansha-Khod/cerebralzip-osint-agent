e4dd388fba520ee8d194040ecce0406caa3c4763
Mansha-Khod <manshakhod@gmail.com>
2026-09-12 13:54:25 +0530

    Add reusable Exa search funtion

---
+++ src/search_tool.py
@@ -0,0 +1,12 @@
+import os
+from exa_py import Exa
+
+exa=Exa(api_key=os.getenv('EXA_API_KEY'))
+
+def serach_web(query:str,num_results: int=5)->list[dict]:
+    # common use search funtion to search the web using given query and return list of result
+    results=exa.search(query,num_results=num_results)
+    return [
+        {"title":r.title,"url":r.url,"snippet":getattr(r,"text","")}
+        for r in results.results
+    ]
