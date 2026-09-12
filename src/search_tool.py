87599966a785b740670febf57af6b9bfec68d529
Mansha-Khod <manshakhod@gmail.com>
2026-09-12 14:26:39 +0530

    Fix typo: serach_web -> search_web

--- src/search_tool.py
+++ src/search_tool.py
@@ -3,7 +3,7 @@ from exa_py import Exa
 
 exa=Exa(api_key=os.getenv('EXA_API_KEY'))
 
-def serach_web(query:str,num_results: int=5)->list[dict]:
+def search_web(query:str,num_results: int=5)->list[dict]:
     # common use search funtion to search the web using given query and return list of result
     results=exa.search(query,num_results=num_results)
     return [
